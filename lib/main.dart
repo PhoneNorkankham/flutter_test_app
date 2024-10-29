7bc0828f5773fabf55bf963bc7ac222eb29fbdd0
Gao Peiris <peiris@shabbat.com>
2024-10-29 13:14:02 -0400

    Deploy the app

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ void main() {
   runApp(const MyApp());
 }
 
-//building the app.
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -29,19 +27,17 @@ class MyApp extends StatelessWidget {
                     Icons.camera,
                     Icons.photo,
                   ],
-                  builder: (icon, isHovered) {
-                    return AnimatedContainer(
-                      duration: const Duration(milliseconds: 200),
-                      height: isHovered ? 64 : 48,
-                      width: isHovered ? 64 : 48,
+                  builder: (icon) {
+                    if (icon == null) return const SizedBox(width: 64, height: 64);
+                    return Container(
+                      height: 48,
+                      width: 48,
                       margin: const EdgeInsets.all(8),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(8),
-                        color: Colors
-                            .primaries[icon.hashCode % Colors.primaries.length],
+                        color: Colors.primaries[icon.hashCode % Colors.primaries.length],
                       ),
-                      child: Icon(icon,
-                          color: Colors.white, size: isHovered ? 36 : 24),
+                      child: Icon(icon, color: Colors.white, size: 24),
                     );
                   },
                 ),
@@ -54,7 +50,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//Dock of the reorderable[items].
 class Dock extends StatefulWidget {
   const Dock({
     super.key,
@@ -62,119 +57,95 @@ class Dock extends StatefulWidget {
     required this.builder,
   });
 
-  final List<IconData> items;
-  final Widget Function(IconData, bool) builder;
+  final List<IconData?> items;
+  final Widget Function(IconData?) builder;
 
   @override
   State<Dock> createState() => _DockState();
 }
 
 class _DockState extends State<Dock> {
-  late List<IconData> _items = widget.items;
-  IconData? _draggedIcon;
-  int? _draggedIndex;
-  bool _isDragging = false;
-  int _hoverIndex = -1;
+  late final List<IconData?> _items = [...widget.items];
+  final GlobalKey _dockKey = GlobalKey();
+  int _draggedIndex = -1;
+  int _tempIndex = -1;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      key: _dockKey,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
         color: Colors.black12,
       ),
-      height: 72,
-      padding: const EdgeInsets.all(4),
-      child: SingleChildScrollView(
-        // Drag horizontally like Dock
-        scrollDirection: Axis.horizontal,
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(8),
+      child: FittedBox(
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: List.generate(_items.length, (index) {
-            return MouseRegion(
-              onEnter: (_) => setState(() => _hoverIndex = index),
-              onExit: (_) => setState(() => _hoverIndex = -1),
-              child: Draggable<IconData>(
-                key: ValueKey(_items[index]),
-                data: _items[index],
-                onDragStarted: () {
+            return Draggable<IconData>(
+              key: ValueKey(_items[index].hashCode),
+              data: _items[index],
+              onDragStarted: () {
+                setState(() {
+                  _draggedIndex = index;
+                  _tempIndex = index;
+                  _items.insert(_tempIndex, null);
+                });
+              },
+              onDraggableCanceled: (_, __) {
+                setState(() {
+                  _items.remove(null);
+                  _draggedIndex = -1;
+                  _tempIndex = -1;
+                });
+              },
+              onDragEnd: (_) {
+                setState(() {
+                  _items.remove(null);
+                  if (_tempIndex >= 0 && _draggedIndex >= 0 && _draggedIndex != _tempIndex) {
+                    final oldItem = _items[_draggedIndex];
+                    _items.removeAt(_draggedIndex);
+                    _items.insert(
+                        _tempIndex < _draggedIndex ? _tempIndex : _tempIndex - 1, oldItem);
+                  }
+                  _draggedIndex = -1;
+                  _tempIndex = -1;
+                });
+              },
+              onDragUpdate: (details) {
+                final renderBox = _dockKey.currentContext?.findRenderObject() as RenderBox?;
+                if (renderBox != null) {
                   setState(() {
-                    _isDragging = true;
-                    _draggedIcon = _items[index];
-                    _draggedIndex = index;
+                    Offset position = renderBox.localToGlobal(Offset.zero);
+                    Size size = renderBox.size;
+                    double dx = details.localPosition.dx - position.dx;
+                    double dy = details.localPosition.dy - position.dy;
+                    bool insideDock = dx > 0 && dx < size.width && dy > 0 && dy < size.height;
+                    int newIndex = (dx / size.width * widget.items.length).toInt();
+                    newIndex = newIndex < _draggedIndex ? newIndex : newIndex + 1;
+                    if (insideDock && newIndex != _draggedIndex && _draggedIndex >= 0) {
+                      if (_tempIndex == newIndex) {
+                        return;
+                      }
+                      _tempIndex = newIndex;
+                      _items.remove(null);
+                      _items.insert(_tempIndex, null);
+                    } else {
+                      _tempIndex = -1;
+                      _items.remove(null);
+                    }
                   });
-                },
-                onDraggableCanceled: (_, __) {
-                  setState(() {
-                    _draggedIcon = null;
-                    _draggedIndex = null;
-                    _isDragging = false;
-                  });
-                },
-                onDragEnd: (_) {
-                  setState(() {
-                    _draggedIcon = null;
-                    _draggedIndex = null;
-                    _isDragging = false;
-                  });
-                },
-                feedback: Material(
-                  color: Colors.transparent,
-                  child: AnimatedScale(
-                    scale: 1.2,
-                    duration: const Duration(milliseconds: 300),
-                    child: widget.builder(_items[index], true),
-                  ),
-                ),
-                childWhenDragging: const SizedBox.shrink(),
-                child: AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 300),
-                  child: GestureDetector(
-                    key: ValueKey(_items[index]),
-                    onTap: () {
-                      if (!_isDragging) return;
-                    },
-                    child: CustomReorderableDelayedDragStartListener(
-                      index: index,
-                      child:
-                          widget.builder(_items[index], _hoverIndex == index),
-                    ),
-                  ),
-                ),
-              ),
+                }
+              },
+              feedback: widget.builder(_items[index]),
+              childWhenDragging: const SizedBox.shrink(),
+              child: widget.builder(_items[index]),
             );
           }),
         ),
       ),
-    );
-  }
-}
-
-class CustomReorderableDelayedDragStartListener extends StatelessWidget {
-  // Creates a listener for an drag following a long press event over the
-  // given child widget.
-  //
-  // This is most commonly used to wrap an entire list item in a reorderable
-  // list.
-  const CustomReorderableDelayedDragStartListener({
-    super.key,
-    required this.index,
-    required this.child,
-  });
-
-  final int index;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return LongPressDraggable<int>(
-      data: index,
-      feedback: Material(
-        color: Colors.transparent,
-        child: child,
-      ),
-      childWhenDragging: const SizedBox.shrink(),
-      child: child,
     );
   }
 }
